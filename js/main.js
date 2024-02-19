9728ce5e309261aba8c72df4f67e554a3908851e
Alex Tayron <57293582+AlexTayron@users.noreply.github.com>
2024-02-19 10:07:53 -0300

    Correção de formulário

--- js/main.js
+++ js/main.js
@@ -1,22 +1,13 @@
 $(document).ready(function () {
 
-  $("#nome").mask("", {
-    placeholder: "Digite seu Nome Completo",
-  });
   $("#cpf").mask("000.000.000-00", {
     placeholder: "000.000.000-00",
   });
   $("#telefone").mask("(00) 00000 - 0000", {
     placeholder: "(11) 99999 - 8888",
   });
-  $("#email").mask("", {
-    placeholder: "seuemail@email.com",
-  });
   $("#cep").mask("00000-000", {
     placeholder: "00000-000",
-  });
-  $("#endereco").mask("", {
-    placeholder: "Digite seu endereço completo",
   });
   $("form").validate({
     rules: {
